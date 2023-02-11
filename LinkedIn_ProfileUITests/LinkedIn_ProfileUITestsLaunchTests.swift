9a6f9238f5b170e04d3932b9c23e2403d0245c15
Atdhe Boshnjaku <atdhe.boshnjaku@gmail.com>
2023-02-11 11:13:50 +0100

    Initial Commit

---
+++ LinkedIn_ProfileUITests/LinkedIn_ProfileUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LinkedIn_ProfileUITestsLaunchTests.swift
+//  LinkedIn_ProfileUITests
+//
+//  Created by Atdhe Boshnjaku on 2/11/23.
+//
+
+import XCTest
+
+final class LinkedIn_ProfileUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
